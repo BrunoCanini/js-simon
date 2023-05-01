eccaf464bd58e298eb0808e0614e6e871635ba4d
Bruno Canini <brunocanini1994@gmail.com>
2023-05-01 19:11:32 +0200

    debud invisible

--- main.js
+++ main.js
@@ -24,15 +24,29 @@ btnStart.addEventListener("click" , function(){
 
     }
 
-    let secondi = 3
+    let secondForInvisible = 3
+    let secondToPrompt = 4
+
     let timer = setInterval( function(){
         
-        if(secondi === 0){
+        if(secondForInvisible === 0){
             clearInterval(timer)
-            box.classList.add("invisible");
+            contenitore.classList.add("invisible");
         } else{
-            console.log(secondi)
-            secondi--
+            console.log(secondForInvisible)
+            secondForInvisible--
+        }
+
+    }, 1 * 1000)
+
+    let timer2 = setInterval( function(){
+        
+        if(secondToPrompt === 0){
+            clearInterval(timer2)
+            let prom = prompt("inserisci i numeri ")
+        } else{
+            console.log(secondToPrompt)
+            secondToPrompt--
         }
 
     }, 1 * 1000)
